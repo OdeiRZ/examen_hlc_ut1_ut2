d1b976bdf2d5fa116820a07740e2e2d2b6661598
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2014-11-06 19:34:19 +0100

    Mejorada funcionalidad de Aplicación

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
-    session_name('listadoTareas');
+    session_name('listado');
     session_start();
-    
+	
     if (isset($_POST['limpiar'])) {
         session_destroy();
         session_start();
@@ -13,23 +13,27 @@
     if (isset($_POST['anadir'])) {
 		if ($_POST["tarea"] != "") {
 			array_push($_SESSION['tareas'], $_POST["tarea"]);
-			$_SESSION['tareasCheck'][count($_SESSION['tareas'])-1] == 0;
+			array_push($_SESSION['tareasCheck'], 0);
 		}
     }
     if (isset($_POST['guardar'])) {
-		for($i = 0; $i < count($_SESSION['tareas']); $i++) {
-			if ($_POST["tareaCheckbox".$i] == "on") {
-				$_SESSION['tareasCheck'][$i] = 1;
-			} else {
+		for ($i = 0; $i < count($_SESSION['tareas']); $i++) {
+			if (isset($_POST["tareaCheckbox".$i])) {
+				if ($_POST["tareaCheckbox".$i] != -1) {
+					$_SESSION['tareasCheck'][$i] = 1;
+				}
+			} elseif ($_SESSION['tareasCheck'][$i] != -1) {
 				$_SESSION['tareasCheck'][$i] = 0;
 			}
 		}
     }
     if (isset($_POST['eliminar'])) {
-		for($i = 0; $i < count($_SESSION['tareas']); $i++) {
-			if ($_POST["tareaCheckbox".$i] != "on") {
-				array_pop($_SESSION['tareas'][$i]);		//no recuerdo el metodo para eliminarlo del array
-				array_pop($_SESSION['tareasCheck'][$i]);//y corte la ayuda offline de php
+		for ($i = 0; $i < count($_SESSION['tareas']); $i++) {
+			if (isset($_POST["tareaCheckbox".$i])) {
+				if ($_POST["tareaCheckbox".$i] == "on") {
+					$_SESSION['tareas'][$i] = -1;
+					$_SESSION['tareasCheck'][$i] = -1;
+				}
 			}
 		}
     }
@@ -38,45 +42,80 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title></title>
+		<title>Agenda de Tareas</title>
 		<style>
-		ul
-		{
-			list-style:square;
-			color:green;
-		}
+			body {
+				background-color:green;
+				color:brown;
+			}
+			h2 {
+				font-size:23px;
+			}
+			h2 + h2 {
+				margin-top:-15px;
+			}
+			ul {
+				list-style:square;
+			}
+			li {
+				margin-left:-10px;
+			}
+			div {
+				background-color:white;
+				border:4px outset brown;
+				margin:30px auto;
+				padding:0px 20px 10px 20px;
+				width:290px;
+			}
+			input[type=checkbox] {
+				vertical-align:middle;
+			}
+			input[type=text] {
+				width:180px;
+			}
 		</style>
-	<head>
+	</head>
 	<body>
 		<form action="index.php" method="post">
+			<div>
 <?php
-			if (count($_SESSION['tareas']) == 0) {
-				echo "No hay tareas. Puede añadir una más abajo.<br/><br/>";
-			} else {
-				echo "<ul>";
-				for($i = 0; $i < count($_SESSION['tareas']); $i++) {
-					$checked = ($_SESSION['tareasCheck'][$i] == 1) ? "checked" : "";
-					echo "<li><input type='checkbox' name='tareaCheckbox".$i."' ".$checked.">";
-					echo "".$_SESSION['tareas'][$i]."</li>";
+				$borradas = count(array_keys($_SESSION['tareasCheck'], -1));
+				if (count($_SESSION['tareas']) == 0 || $borradas == count($_SESSION['tareas'])) {
+					echo "\t\t\t\t<h2>No hay tareas.</h2><h2>Puede añadir una más abajo</h2>\n";
+				} else {
+					echo "\t\t\t\t<h2>Listado de Tareas</h2>\n";
+					echo "\t\t\t<ul>\n";
+					for ($i = 0; $i < count($_SESSION['tareas']); $i++) {
+						if (isset($_SESSION['tareasCheck'][$i])) {
+							if ($_SESSION['tareasCheck'][$i] != -1) {
+								$id = "tareaCheckbox".$i;
+								$checked = ($_SESSION['tareasCheck'][$i] == 1) ? "checked" : "";
+								echo "\t\t\t\t<li><label for='".$id."'>";
+								echo "<input type='checkbox' name='".$id."' id='".$id."' ".$checked.">";
+								echo "<i>".$_SESSION['tareas'][$i]."</i></label></li>\n";
+							}
+						}
+					}
+					echo "\t\t\t</ul>\n";
+					echo "\t\t\t<p><input type='submit' name='guardar' value='Guardar'></p>\n";
 				}
-				echo "</ul>";
-			}
 ?>
-			<input type="submit" name="guardar" value="Guardar"><br/><br/>
-            <input type="text" name="tarea" placeholder="Introduce una tarea">
-            <input type="submit" name="anadir" value="Añadir tarea">
-			<hr/>
+			<p>
+				<input type="text" name="tarea" placeholder="Introduce una tarea">
+				<input type="submit" name="anadir" value="Añadir tarea">
+			</p>
 <?php
-			if (count($_SESSION['tareas']) > 0) {
-				$errores = count($_SESSION['tareas']);
-				$cont1 = count(array_diff($_SESSION['tareasCheck'], 1));
-				$s = (count($_SESSION['tareas']) > 1) ? "s" : "";
-				echo $cont1;
-				echo "<br/>Hay ".$cont1." tarea".$s." pendiente".$s." de ".$errores." en total<br/><br/>";
-			}
+				if ($borradas != count($_SESSION['tareas'])) {
+					$total = count($_SESSION['tareas']) - $borradas;
+					$cont1 = count(array_keys($_SESSION['tareasCheck'], 1));
+					$s = ($cont1 != 1) ? "s" : "";
+					echo "\t\t\t<hr/>\n\t\t\t<p>Hay <b>".$cont1."</b> tarea".$s;
+					echo "pendiente".$s." de <b>".$total."</b> en total</p>";
+					echo "<p><input type='submit' name='eliminar' value='Eliminar tareas completadas'> ";
+					echo "<input type='submit' name='limpiar' value='Vaciar lista'></p>";
+				}
 ?>
-			<input type="submit" name="eliminar" value="Eliminar tareas completadas">
-			<input type="submit" name="limpiar" value="Vaciar la lista">
+			</div>
         </form>
 	</body>
 </html>
